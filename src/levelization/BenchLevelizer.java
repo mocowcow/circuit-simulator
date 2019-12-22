319c408383cb13636d9f364dd54c43d4c214a6b8
405630954 <405630954@vd.tku.edu.tw>
2019-12-22 21:28:08 +0800

    implements levelization

--- src/levelization/BenchLevelizer.java
+++ src/levelization/BenchLevelizer.java
@@ -6,12 +6,13 @@ import java.io.*;
 public class BenchLevelizer {
 
     //configs
-    final static String UX_BENCH_FILE = "c:/ISCAS85/bench/c17_UX.bench.txt";
+    final static String UX_BENCH_FILE = "c:/ISCAS85/bench/c432_UX.bench.txt";
     final static String LEVELIZED_BENCH_FILE = UX_BENCH_FILE.replace("UX", "LEVELIZED");
     //variables
     static LinkedList<String> outputList = new LinkedList<>();
-    static LinkedList<String> unlevelizedList = new LinkedList<>();
-    static LinkedList<String> levelizedList = new LinkedList<>();
+    static LinkedList<Gate> unready = new LinkedList<>();
+    static LinkedList<Gate> ready = new LinkedList<>();
+    static HashMap<String, Gate> gates = new HashMap<>();
 
     public static void main(String[] args) {
         readUXBench(UX_BENCH_FILE);
@@ -26,12 +27,17 @@ public class BenchLevelizer {
             while (null != (aLine = br.readLine())) {
                 if (aLine.startsWith("#") || aLine.startsWith("OUTPUT(") || aLine.length() == 0) {
                     outputList.add(aLine);
-                } else if (aLine.startsWith("INPUT(")) {
-                    outputList.add(aLine);
-                    unlevelizedList.add(aLine);
-                } else {
-                    unlevelizedList.add(aLine);
+                    continue;
                 }
+                Gate g;
+                if (aLine.startsWith("INPUT(")) {
+                    outputList.add(aLine);
+                    g = Gate.getInputGate(aLine);
+                } else {
+                    g = Gate.getGate(aLine);
+                    unready.add(g);
+                }
+                gates.put(g.name, g);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -39,7 +45,28 @@ public class BenchLevelizer {
     }
 
     private static void start() {
-      
+        while (!unready.isEmpty()) {
+            Gate g = unready.poll();
+            int maxLevel = 0;
+            boolean isReady = true;
+            //push unready gate back
+            for (String k : g.inputs) {
+                int n = gates.get(k).level;
+                if (n == 0) {
+                    unready.add(g);
+                    isReady = false;
+                    break;
+                }
+                if (n > maxLevel) {
+                    maxLevel = n;
+                }
+            }
+            //set level to this gate and push to done
+            if (isReady) {
+                g.level = maxLevel + 1;
+                ready.add(g);
+            }
+        }
     }
 
     private static void writeLevelizedBeanch(String path) {
@@ -48,14 +75,47 @@ public class BenchLevelizer {
                 bw.write(k);
                 bw.newLine();
             }
-            for (String k : levelizedList) {
-                bw.write(k);
-                bw.newLine();
+            //output gates
+            for (Gate g : ready) {
+                System.out.println(g.name + " " + g.level);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
 
         }
     }
+}
 
+class Gate {
+
+    final String originalString;
+    final String name;
+    String[] inputs;
+    int level;
+
+    private Gate(String s, String n) {
+        originalString = s;
+        name = n;
+    }
+
+    private Gate(String s, String n, int level) {
+        this(s, n);
+        this.level = level;
+    }
+
+    public static Gate getInputGate(String s) {
+        return new Gate(s, s.substring(6, s.length() - 1), 1);
+    }
+
+    public static Gate getGate(String s) {
+        Gate g = new Gate(s, s.substring(0, s.indexOf(" ")));
+        s = s.substring(s.indexOf("(") + 1, s.length() - 1);
+        g.inputs = s.split(", ");
+        return g;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("name : %s , str : %s", name, originalString);
+    }
 }
